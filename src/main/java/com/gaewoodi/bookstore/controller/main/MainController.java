960dbd7690157ea5ea610d2d99de8d4ac2e6243f
heoyool <logenbox21@gmail.com>
2023-07-07 18:07:42 +0900

    main.html작성중

---
+++ src/main/java/com/gaewoodi/bookstore/controller/main/MainController.java
@@ -0,0 +1,14 @@
+package com.gaewoodi.bookstore.controller.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    public String main() {
+        return "main/index";
+    }
+}
---
+++ src/main/resources/static/js/main/js.js
@@ -0,0 +1,6 @@
+
+
+// 메뉴나타나는 js
+    function popup() {
+        document.querySelector("menu-menu").display="block"
+    }
