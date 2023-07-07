a886b87d130dd869bbb5fc864c35a1b065fc1369
DESKTOP-NUOPUIF\ITPS <dndpgml@naver.com>
2023-07-07 09:08:55 +0900

    purchase_list html 구성중

---
+++ src/main/java/com/gaewoodi/bookstore/controller/purchase/PurchaseController.java
@@ -0,0 +1,14 @@
+package com.gaewoodi.bookstore.controller.purchase;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/purchase")
+public class PurchaseController {
+    @GetMapping("/list")
+    public String getPurchaseList() {
+        return "purchase/purchase_list";
+    }
+}
