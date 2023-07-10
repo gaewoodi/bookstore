a24aa4135631424b2f519891bb1e752dff5fcd50
xldkf369 <dydwk12304@naver.com>
2023-07-10 17:52:03 +0900

    no message

--- src/main/java/com/gaewoodi/bookstore/controller/admin/DashboardController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/admin/DashboardController.java
@@ -2,11 +2,13 @@ package com.gaewoodi.bookstore.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AdminController {
+@RequestMapping("/admin")
+public class DashboardController {
 
-    @GetMapping("/admin")
+    @GetMapping("/dashboard")
     public String getAdmin(){
         return "admin/admin_main/admin";
     }
