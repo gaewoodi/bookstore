d1674dcea921d2c87bb33322df3270dce897bd5e
smile6060 <dndpgml@naver.com>
2023-07-12 12:38:51 +0900

    ** bookDto 추가 (공용으로 사용할거라 폴더화 X)

--- src/main/java/com/gaewoodi/bookstore/dto/bookDto.java
+++ src/main/java/com/gaewoodi/bookstore/dto/bookDto.java
@@ -1,2 +1,17 @@
-package com.gaewoodi.bookstore.dto;public class bookDto {
+package com.gaewoodi.bookstore.dto;
+
+import lombok.Data;
+
+import java.time.Year;
+
+@Data
+public class bookDto {
+    private int bookId;
+    private String bookCode;
+    private String bookName;
+    private String author;
+    private String publisher;
+    private Year publicationDate;
+    private String category;
+    private String price;
 }
