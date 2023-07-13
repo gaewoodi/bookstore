cb4cf55bab3772bd6a9233c35512e0efe181822f
xldkf369 <dydwk12304@naver.com>
2023-07-13 17:38:12 +0900

    페이지 추가 , 사이드바js, css수정

--- src/main/java/com/gaewoodi/bookstore/controller/admin/AccountController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/admin/AccountController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccountController {
 
-    @GetMapping("/admin/Account")
+    @GetMapping("/admin/account")
     public String urlAccount(){
         return "admin/admin_Account";
     }
