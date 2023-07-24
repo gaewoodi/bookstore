b4b6032d73fe8df4014ffe99ef2fec17dcd9ce67
xldkf369 <dydwk12304@naver.com>
2023-07-24 16:50:11 +0900

    색조절

--- src/main/resources/sql/admin/inventory.sql
+++ src/main/resources/sql/admin/inventory.sql
@@ -1,7 +1,7 @@
 use bookstore ;
 
 create table inventory (
-    도서번호	(책 코드) book_code varchar(45) not null,
+    도서번호	(책 코드) book_id varchar(45) not null,
     도서명	(책 이름) book_name varchar(45) not null,
     창고재고	(책 수량) book_count int not null,
     주문대기(주문과 연계)
@@ -9,3 +9,4 @@ create table inventory (
     재고수정
     판매상황	(sell/ sell out)
 );
+
