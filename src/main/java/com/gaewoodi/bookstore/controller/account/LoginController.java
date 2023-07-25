3bc9b5dafd087b3d8f61f4cc9c3a885883ed1220
wlals0476 <wldbs0476@naver.com>
2023-07-25 16:54:12 +0900

    no message

--- src/main/java/com/gaewoodi/bookstore/controller/account/LoginController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/account/LoginController.java
@@ -1,2 +1,22 @@
-package com.gaewoodi.bookstore.controller.account;public class LoginController {
+package com.gaewoodi.bookstore.controller.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "account/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "account/register";
+    }
+
+
 }
