17dadd1b5ec90d2ecb4a641cadbea7d708cd7968
heoyool <logenbox21@gmail.com>
2023-07-26 16:58:37 +0900

    7.26일본

--- src/main/resources/static/js/main/js.js
+++ src/main/resources/static/js/main/js.js
@@ -46,13 +46,56 @@
     
         }
 
+    // 메뉴이동
+    // const tabList = document.querySelectorAll('main6-menu > ul > a > .one');
+        
+    // for(let i = 0; i < tabList.length; i++){
+    //     tabList[i].querySelector('.one').addEventListener('click', function(e){
+    //     e.preventDefault();
+    //     for(let j = 0; j < tabList.length; j++){
+    //         tabList[j].classList.remove('main6-list1');
+    //     }
+    //     this.parentNode.classList.add('main6-list1');
+    //     });
+    // }
+    
+    // for(let i=0; i<tabList.length; i++) {
+    //     tabList[i].querySelector('.one').addEventListener('click', function(e) {
+    //         e.preventDefault();
+    
+    //         for(let j=0; j<tabList.length; j++) {
+    //             tabList[j].classList.remove();
+    //         } 
+    //     });
+    // }
 
-        // 카테고리변경
-        function menu2one() {
-            if(document.querySelector(".main6-list1").style.display === "block") {
-                document.querySelector(".main6-list1").style.display = "none"
-            }
+
+
+
+
+
+
+    // 탭전환 자바스크립트
+    const tabList = document.querySelectorAll('.main .container .main6 .main6-secondwrap .main6-menu > ul > li');
+    const contents = document.querySelectorAll('.main .container .main6 .main6-secondwrap .main6-menu .main6-list')
+    let activeCont = '#one'; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+  
+    for(var i = 0; i < tabList.length; i++){
+      tabList[i].querySelector('.btn').addEventListener('click', function(e){
+        e.preventDefault();
+        for(var j = 0; j < tabList.length; j++){
+          // 나머지 버튼 클래스 제거
+          tabList[j].classList.remove('is_on');
+  
+          // 나머지 컨텐츠 display:none 처리
+          contents[j].style.display = 'none';
         }
-
-
-        // 메뉴나타나는 js
+  
+        // 버튼 관련 이벤트
+        this.parentNode.classList.add('is_on');
+  
+        // 버튼 클릭시 컨텐츠 전환
+        activeCont = this.getAttribute('href');
+        document.querySelector(activeCont).style.display = 'block';
+    });
+}
