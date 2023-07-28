0b1cd4249c6cca2a3faaa74e3214fafe3483edec
wlals0476 <wldbs0476@naver.com>
2023-07-28 16:45:02 +0900

    login

--- src/main/java/com/gaewoodi/bookstore/controller/account/LoginController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/account/LoginController.java
@@ -1,31 +1,28 @@
 package com.gaewoodi.bookstore.controller.account;
 
+import com.gaewoodi.bookstore.dto.account.RegisterDto;
+import com.gaewoodi.bookstore.mappers.account.LoginMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private LoginMapper loginMapper;
 
     @GetMapping("/login")
     public String getLogin() {
         return "account/login";
     }
 
-<<<<<<< HEAD
     @PostMapping("/login")
     public String checkLogin(@ModelAttribute RegisterDto registerDto) {
         System.out.println(loginMapper.getBookInfo(registerDto));
 
         return "account/login";
     }
-=======
-    @GetMapping("/register")
-    public String getRegister() {
-        return "account/register";
-    }
-
-
->>>>>>> parent of 0999fcc (no message)
 }
