aa1fe60f3366e98d8ce3ae0f265474c06526c7fa
xldkf369 <dydwk12304@naver.com>
2023-07-31 15:20:52 +0900

    sql 수정

--- src/main/resources/sql/admin/register.sql
+++ src/main/resources/sql/admin/register.sql
@@ -1,23 +1,25 @@
 use bookstore;
 
-create table book_register(
-book_reg_id int not null auto_increment,
-book_reg_userid varchar(20),
-book_reg_passwd varchar(20) not null,
-book_reg_name varchar(10) not null,
-book_reg_birth date,
-book_reg_gender char(1),
-book_reg_email varchar(50) not null unique,
-book_reg_adr varchar(255),
-book_reg_tel varchar(15),
-book_reg_level int default 1,
-book_reg_grade varchar(50),
-book_reg_created datetime,
-book_reg_modified datetime,
-book_reg_image_name varchar(255),
-book_reg_image_size bigint,
-book_reg_trans_name varchar(255),
-primary key(book_reg_id)
+create table register(
+reg_id int not null auto_increment,
+    reg_userid varchar(20),
+    reg_passwd varchar(20) not null,
+    reg_name varchar(10) not null,
+    reg_birth date,
+    reg_gender char(1),
+    reg_email varchar(50) not null unique,
+    reg_adr varchar(255),
+    reg_tel1 int(3),
+    reg_tel2 int(4),
+    reg_tel3 int(4),
+    reg_level int default 1,
+    reg_grade varchar(50),
+    reg_created datetime,
+    reg_modified datetime,
+    reg_image_name varchar(255),
+    reg_image_size bigint,
+    reg_trans_name varchar(255),
+    primary key(book_reg_id)
 );
 
-INSERT INTO book_register VALUES(NULL, 'admin', '0824', '관리자', now(), 'M', 'admin@mail.com', NULL, '010-1111-1111', '9', null, now(), now(), '', 0, '');
+INSERT INTO book_register VALUES(NULL, 'admin', '0824', '관리자', now(), 'M', 'admin@mail.com', NULL, '010', '1234', '4567', '9', null, now(), now(), '', 0, '');
