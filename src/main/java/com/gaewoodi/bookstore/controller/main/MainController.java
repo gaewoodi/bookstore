986c5af1d9a9cebac781faded88c2c77a6246d5a
heoyool <logenbox21@gmail.com>
2023-08-02 16:52:51 +0900

    no message

--- src/main/java/com/gaewoodi/bookstore/controller/main/MainController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/main/MainController.java
@@ -10,5 +10,4 @@ public class MainController {
     public String getMain() {
         return "main/index";
     }
-
 }
--- src/main/resources/static/js/main/js.js
+++ src/main/resources/static/js/main/js.js
@@ -46,56 +46,22 @@
     
         }
 
-    // 메뉴이동
-    // const tabList = document.querySelectorAll('main6-menu > ul > a > .one');
+        // 탭메뉴
+        $(document).ready(function () {
+
+            $('.main6-menu ul li').click(function () {
+                var tab_id = $(this).attr('data-tab');
+
+                $('.main6-menu ul li').removeClass('active');
+                $('.tab-content').removeClass('active');
+
+                //tab-content의 내용을 display:none으로 처리해 두고 탭 버튼을 누르면 해당 tab-content에 active.
+                $(this).addClass('active');
+                $("#" + tab_id).addClass('active');
+            })
+
+        })
+
+
+
         
-    // for(let i = 0; i < tabList.length; i++){
-    //     tabList[i].querySelector('.one').addEventListener('click', function(e){
-    //     e.preventDefault();
-    //     for(let j = 0; j < tabList.length; j++){
-    //         tabList[j].classList.remove('main6-list1');
-    //     }
-    //     this.parentNode.classList.add('main6-list1');
-    //     });
-    // }
-    
-    // for(let i=0; i<tabList.length; i++) {
-    //     tabList[i].querySelector('.one').addEventListener('click', function(e) {
-    //         e.preventDefault();
-    
-    //         for(let j=0; j<tabList.length; j++) {
-    //             tabList[j].classList.remove();
-    //         } 
-    //     });
-    // }
-
-
-
-
-
-
-
-    // 탭전환 자바스크립트
-    const tabList = document.querySelectorAll('.main .container .main6 .main6-secondwrap .main6-menu > ul > li');
-    const contents = document.querySelectorAll('.main .container .main6 .main6-secondwrap .main6-menu .main6-list')
-    let activeCont = '#one'; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
-  
-    for(var i = 0; i < tabList.length; i++){
-      tabList[i].querySelector('.btn').addEventListener('click', function(e){
-        e.preventDefault();
-        for(var j = 0; j < tabList.length; j++){
-          // 나머지 버튼 클래스 제거
-          tabList[j].classList.remove('is_on');
-  
-          // 나머지 컨텐츠 display:none 처리
-          contents[j].style.display = 'none';
-        }
-  
-        // 버튼 관련 이벤트
-        this.parentNode.classList.add('is_on');
-  
-        // 버튼 클릭시 컨텐츠 전환
-        activeCont = this.getAttribute('href');
-        document.querySelector(activeCont).style.display = 'block';
-    });
-}
