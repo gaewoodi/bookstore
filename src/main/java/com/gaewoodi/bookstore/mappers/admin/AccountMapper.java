da41ae15d9f077357ac5d38890fc0370849cd30c
xldkf369 <dydwk12304@naver.com>
2023-08-03 16:48:23 +0900

    no message

--- src/main/java/com/gaewoodi/bookstore/mappers/admin/AccountMapper.java
+++ src/main/java/com/gaewoodi/bookstore/mappers/admin/AccountMapper.java
@@ -12,35 +12,10 @@ import java.util.List;
 @Mapper
 public interface AccountMapper {
 
-    @Insert("INSERT INTO register VALUES(NULL, " +
-            "#{regUserid}, " +
-            "#{regPasswd}, " +
-            "#{regName}, " +
-            "now(), " +
-            "'M', " +
-            "#{regEmail}, " +
-            "NULL, " +
-            "#{regTel}, '1', null, now(), now(), '', 0, '')")
-    void getRegister(RegisterDto registerDto);
-
     @Select("SELECT * FROM register ORDER BY reg_id DESC")
     List<RegisterDto> getMemberAll();
 
     @Select("SELECT * FROM register WHERE reg_id = #{regId}")
     RegisterDto getMemberOne(int id);
-
-    @Update("UPDATE register SET " +
-            "reg_userid = #{regUserid}, " +
-            "reg_passwd = #{regPasswd}, " +
-            "reg_name = #{regName}, " +
-            "reg_email = #{regEmail}, " +
-            "reg_adr = #{regAdr}, " +
-            "reg_tel=#{regTel}, " +
-            "reg_level=#{regLevel}, " +
-            "reg_grade=#{regGrade}, " +
-            "reg_modified= NOW() " +
-            "reg_id = #{regId}")
-    void setAccountUpdate(RegisterDto registerDto);
-
 
 }
