0425398425eb0555a61d4fab03b7284be3f85901
smile6060 <dndpgml@naver.com>
2023-08-08 23:40:23 +0900

    user edit => update로 변경 side menu 경로수정 user_edit, mypage 구현중

--- src/main/java/com/gaewoodi/bookstore/controller/mypage/MypageController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/mypage/MypageController.java
@@ -1,19 +1,47 @@
 package com.gaewoodi.bookstore.controller.mypage;
 
+import com.gaewoodi.bookstore.dto.account.RegisterDto;
+import com.gaewoodi.bookstore.mappers.mypage.MypageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MypageController {
 
+    @Autowired
+    private MypageMapper mypageMapper;
+
     @GetMapping("/mypage")
-    public String getMypage() {
+    public String getMypage(Model model, @RequestParam int regId) {
+        model.addAttribute("user", mypageMapper.getMypageId(regId));
+
+
         return "mypage/mypage";
     }
 
-    @GetMapping("/user/edit")
-    public String getserEdit() {
+    @GetMapping("/update")
+    public String getserEdit(Model model, @RequestParam int regId) {
+        model.addAttribute("user", mypageMapper.getMypageId(regId));
+
 
         return "mypage/user_edit";
+    }
+
+    @PostMapping("/update")
+    public Map<String, Object> setUpdate(@ModelAttribute RegisterDto registerDto) {
+        Map<String, Object> map = new HashMap<>();
+
+        mypageMapper.setRegister(registerDto);
+
+        map.put("message", "success");
+        return map;
     }
 }
---
+++ src/main/java/com/gaewoodi/bookstore/mappers/mypage/MypageMapper.java
@@ -0,0 +1,17 @@
+package com.gaewoodi.bookstore.mappers.mypage;
+
+import com.gaewoodi.bookstore.dto.account.RegisterDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface MypageMapper {
+
+    @Select("SELECT * FROM register WHERE regId = #{regId}")
+    RegisterDto getMypageId(int regId);
+
+    @Update("UPDATE register SET id = #{id}, passwd = #{passwd}, name = #{name}, birth = #{birth}, gender = #{gender}, email = #{email}, postcode = #{postcode}, address = #{address}, address1 = #{address1}, tel = #{tel}, level = #{level}, grade = #{grade}, modified = now()")
+    void setRegister(RegisterDto registerDto);
+}
+
