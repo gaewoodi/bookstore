20289e15bb43ab203c2e9e8a91b2b3d4754fd763
heoyool <logenbox21@gmail.com>
2023-08-14 09:06:35 +0900

    no message

--- src/main/resources/sql/main/main.sql
+++ src/main/resources/sql/main/main.sql
@@ -2,5 +2,17 @@
 
 use bookstore;
 create table bestseller (
-
+    board_id int not null auto_increment,
+    book_bestseller int,
+    book_newbook int,
+    book_partbest int,
+    book_gawoody int,
+    book_mz int,
+    book_trend int,
+    book_publisher int,
+    book_media int,
+    book_writer int,
+    primary key(board_id)
 );
+
+insert into bestseller values()
