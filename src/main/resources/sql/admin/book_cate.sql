796022b1b83f905571f44e78f61fe7184d9e8fda
xldkf369 <dydwk12304@naver.com>
2023-08-16 16:13:14 +0900

    최신화

--- src/main/resources/sql/admin/book_cate.sql
+++ src/main/resources/sql/admin/book_cate.sql
@@ -5,9 +5,16 @@ create table book_cate(
 
 );
 
+use bookstore;
+
+create table book_cate(
+
+
+);
+
 1차 분리(대분류)
-소설
+소설,역사,가정,유아,인문,자기계발,에세이,IT,정치,기타,진로
 2차 분리(소분류)
-시
-3차 분리(book_mst)
+시,문화,생활,어린이,사회,과학,경제,교육
+3차 분리(book_mst)-카테
 희곡
