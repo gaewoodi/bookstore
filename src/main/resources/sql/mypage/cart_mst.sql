e3945883536775f4f6ce4d6431af5030214e4c3b
smile6060 <dndpgml@naver.com>
2023-08-21 23:49:07 +0900

    delete 구현중

--- src/main/resources/sql/mypage/cart_mst.sql
+++ src/main/resources/sql/mypage/cart_mst.sql
@@ -16,6 +16,9 @@ drop table cart_mst;
 
 DELETE FROM cart_mst WHERE reg_id = #{regId} AND bookId = #{bookId}
 DELETE FROM cart_mst WHERE reg_id = '3' AND bookId = '1';
+
+SELECT COUNT(book_id) as cartTotal FROM cart_mst WHERE reg_id = '2';
+SELECT COUNT(book_id) as cartTotal FROM cart_mst WHERE reg_id = #{regId};
 ---------------------------------------
 
 SELECT cm.book_id, cm.reg_id, cm.create_date, bm.* FROM book_mst bm LEFT OUTER JOIN cart_mst cm ON(bm.book_id = cm.book_id) WHERE bm.book_id = cm.book_id AND cm.reg_id = '2' ORDER BY cm.book_Id;
