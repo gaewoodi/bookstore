475289a7a236350fa0dbffccc83aa5c9f061f723
smile6060 <dndpgml@naver.com>
2023-08-23 16:49:29 +0900

    right box 구현중

--- src/main/resources/static/js/mypage/purchase/right_box.js
+++ src/main/resources/static/js/mypage/purchase/right_box.js
@@ -1,74 +1,76 @@
 let totalPrice = document.querySelector(".totalPrice");
+let price = document.querySelector(".purchase-price");
 
 totalPrice.textContent = 0 + "원"
 
-function rightCheckbox() {
-    const query = 'input[name="rightbox"]:checked';
-    const selectedElements = document.querySelectorAll(query);
-    
-    let priceId = '';
-    selectedElements.forEach((el) => {
-        priceId += el.value + ' ';
-    });
-    
-    console.log("priceId: " + priceId);
+const query = 'input[name="rightbox"]:checked';
+const selectedElements = document.querySelectorAll(query);
 
-    const checkArray = [];
+let priceId = '';
+selectedElements.forEach((el) => {
+    priceId += el.value + ' ';
+});
 
-    $("input[name='rightbox']:checked").each(function(i) {
-        // 체크된 것만 값을 뽑아서 배열에 push
-        checkArray.push($(this).val());     
-    });
+console.log("priceId: " + priceId);
 
-    
- 
-    // ** 버튼 부분                                                                **//
+const checkArray = [];
 
-    let minus = document.querySelector(".minus");
-    let plus = document.querySelector(".plus");
-    let resultElement = document.querySelector("#result");
-    
+$("input[name='rightbox']:checked").each(function(i) {
+    // 체크된 것만 값을 뽑아서 배열에 push
+    checkArray.push($(this).val());     
+});
 
-    let quantity = 1;
 
-    minus.addEventListener("click", () => {
-        if(quantity > 0) {
-            quantity--
-            resultElement.textContent = quantity;
 
-            for (let i = 0; i < checkArray.length; i++) {
-                console.log("마이너스: " + checkArray[i]);
-                let totalPriceNumber = quantity * checkArray[i];
-                totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
-            }
-          
-            // let totalPriceNumber = quantity * priceId;
-            // totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
+// ** 버튼 부분                                                                **//
 
-        }else {
-            totalPrice.textContent = 0 + "원"
-        }
+let minus = document.querySelector(".minus");
+let plus = document.querySelector(".plus");
+let resultElement = document.querySelector("#result");
 
-    });
 
-    plus.addEventListener("click", () => {
-        quantity++
+let quantity = 1;
+
+minus.addEventListener("click", () => {
+    if(quantity > 0) {
+        quantity--
         resultElement.textContent = quantity;
 
         for (let i = 0; i < checkArray.length; i++) {
-            console.log("플러스: " + checkArray[i]);
+            console.log("마이너스: " + checkArray[i]);
             let totalPriceNumber = quantity * checkArray[i];
-            totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
+            price.textContent = totalPriceNumber.toLocaleString() + "원";
+            console.log("total: " + totalPriceNumber);
         }
+        
+        // let totalPriceNumber = quantity * priceId;
+        // totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
 
-    });
+    }else {
+        price.textContent = 0 + "원"
+    }
 
-    // plus.addEventListener("click", () => {
-    //     quantity++
-    //     resultElement.textContent = quantity;
 
-    //     let totalPriceNumber = quantity * checkArray;
-    //     totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
+});
 
-    // });
-}
+plus.addEventListener("click", () => {
+    quantity++
+    resultElement.textContent = quantity;
+
+    for (let i = 0; i < checkArray.length; i++) {
+        console.log("플러스: " + checkArray[i]);
+        let totalPriceNumber = quantity * checkArray[i];
+        price.textContent = totalPriceNumber.toLocaleString() + "원";
+        console.log("total: " + totalPriceNumber);
+    }
+
+});
+
+// plus.addEventListener("click", () => {
+//     quantity++
+//     resultElement.textContent = quantity;
+
+//     let totalPriceNumber = quantity * checkArray;
+//     totalPrice.textContent = totalPriceNumber.toLocaleString() + "원";
+
+// });
