78e933fdd19bb3a09a0abf6bf3b5f7d1c288ce8a
smile6060 <dndpgml@naver.com>
2023-08-23 12:10:07 +0900

    kakaopay 취소시 경로 변경

--- src/main/java/com/gaewoodi/bookstore/controller/mypage/PurchaseController.java
+++ src/main/java/com/gaewoodi/bookstore/controller/mypage/PurchaseController.java
@@ -107,7 +107,7 @@ public class PurchaseController {
             // 커넥션 input output 전해줄게 있는지 없는지
             connection.setDoOutput(true);
 
-            String payData = "cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name=초코파이&quantity=1&total_amount=2200&vat_amount=200&tax_free_amount=0&approval_url=http://localhost:8888/purchase/kakaopay&fail_url=http://localhost:8888/purchase&cancel_url=http://localhost:8888/purchase";
+            String payData = "cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name=초코파이&quantity=1&total_amount=2200&vat_amount=200&tax_free_amount=0&approval_url=http://localhost:8888/purchase/kakaopay&fail_url=http://localhost:8888/purchase&cancel_url=http://localhost:8888/book";
             OutputStream outputStream = connection.getOutputStream();
             DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
             dataOutputStream.writeBytes(payData);
